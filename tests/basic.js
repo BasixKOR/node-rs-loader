afb28f28a0a4201b4c2a0f0d682908b485b69ec6
Basix <basix@basix.tech>
2021-10-29 00:18:03 +0900

    Add tests

--- tests/basic.js
+++ tests/basic.js
@@ -1,8 +1,17 @@
-import test from 'ava';
-import * as helpers from './helpers/index.js';
+import test from "ava";
+import * as helpers from "./helpers/index.js";
 
-test('successfully make a bundle', async t => {
-  const compiler = helpers.getCompiler('basic.js')
-  await helpers.compile(compiler);
-  t.pass();
-})
+test("successfully make a bundle", async (t) => {
+  const compiler = helpers.getCompiler("basic.js");
+  await t.notThrowsAsync(helpers.compile(compiler));
+});
+
+test("bundle includes .node", async (t) => {
+  const compiler = helpers.getCompiler("basic.js");
+  const stats = await helpers.compile(compiler);
+  t.assert(
+    Object.keys(stats.compilation.assets).find((asset) =>
+      asset.endsWith(".node")
+    )
+  );
+});
